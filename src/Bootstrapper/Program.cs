87f77bc9ed51b0e06d17b29a26e652320e889238
Beniamin <beniamin@eklos.ro>
2026-08-20 12:16:03 +0300

    feat: Data Management of Modular Monolith: Setup Postgres as a Main Database

--- src/Bootstrapper/Program.cs
+++ src/Bootstrapper/Program.cs
@@ -1,35 +1,33 @@
-namespace Eshop.Api;
-
-public class Program
+namespace Eshop.Api
 {
-    private static void Main(string[] args)
+    internal static class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
-
-        builder.Services
-            .AddCatalogModule(builder.Configuration)
-            .AddBasketModule(builder.Configuration)
-            .AddOrderingModule(builder.Configuration);
-
-
-        var app = builder.Build();
-
-        app.UseCatalogModule()
-           .UseBasketModule()
-           .UseOrderingModule();
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
+        public static void Main(string[] args)
         {
-            app.MapOpenApi();
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            builder.Services
+                .AddCatalogModule(builder.Configuration)
+                .AddBasketModule(builder.Configuration)
+                .AddOrderingModule(builder.Configuration);
+
+            var app = builder.Build();
+
+            app.UseCatalogModule()
+               .UseBasketModule()
+               .UseOrderingModule();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.Run();
         }
-
-        //  app.UseHttpsRedirection();
-
-        app.Run();
     }
 }
